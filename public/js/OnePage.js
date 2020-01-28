d106308d55553c6195e035818a5afbf3bbc3157c
Amandine <amandine2.divay@gmail.com>
2020-01-28 21:46:20 +0100

    animation section piliers + corrections

--- public/js/OnePage.js
+++ public/js/OnePage.js
@@ -18,8 +18,9 @@ window.onload = function() {
         document.getElementById("bienEtre").style.display = "flex";
         document.getElementById("bienManger").style.display = "none"; 
         document.getElementById("bienMangerLight").style.display = "flex"; 
+        document.getElementById("bio").style.display = "none";
+        document.getElementById("bioLight").style.display = "flex";
         document.getElementById("explication-bienManger").style.opacity = "0";
-        // document.getElementById("explication-bienManger").style.display = "none";
         document.getElementById("explication-bienEtre").style.opacity = "1";
     } 
 
@@ -33,5 +34,18 @@ window.onload = function() {
         document.getElementById("explication-bienManger").style.opacity = "1";
     }
 
+    document.getElementById("bienMangerLight").onmouseout = function (){
+        document.getElementById("bienManger").style.display = "flex";
+        document.getElementById("explication-bienManger").style.opacity = "0";
+    }
 
+    document.getElementById("bioLight").onmouseover = function (){
+        document.getElementById("bio").style.display = "flex"; 
+        document.getElementById("explication-biodiversité").style.opacity = "1";
+    }
+
+    document.getElementById("bioLight").onmouseout = function (){
+        document.getElementById("bio").style.display = "none";
+        document.getElementById("explication-biodiversité").style.opacity = "0";
+    }
 }
