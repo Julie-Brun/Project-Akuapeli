a391490fdab3d6a21b45d6e6c89d7865988553cc
Amandine <amandine2.divay@gmail.com>
2020-01-28 16:22:18 +0100

    responsive section piliers + animations

---
+++ public/js/OnePage.js
@@ -0,0 +1,37 @@
+// Animations feuilles Les Piliers 
+
+window.onload = function() {
+    // const leafBienEtre = document.getElementById ("bienEtre"); 
+    // const leafBienManger = document.getElementById ("bienMnager"); 
+    // const leafBio = document.getElementById ("bio"); 
+
+    // const leafBienEtreLight = document.getElementById ("bienEtreLight"); 
+    // const leafBienMangerLight = document.getElementById ("bienMangerLight"); 
+    // const leafBioLight = document.getElementById ("bioLight"); 
+
+    // const bienEtreTexte = document.getElementById ("explication-bienEtre")
+    // const bienMangerTexte = document.getElementById ("explication-bienManger"); 
+    // const bioTexte = document.getElementById ("explication-biodiversité"); 
+
+
+    document.getElementById("bienEtreLight").onmouseover = function (){
+        document.getElementById("bienEtre").style.display = "flex";
+        document.getElementById("bienManger").style.display = "none"; 
+        document.getElementById("bienMangerLight").style.display = "flex"; 
+        document.getElementById("explication-bienManger").style.opacity = "0";
+        // document.getElementById("explication-bienManger").style.display = "none";
+        document.getElementById("explication-bienEtre").style.opacity = "1";
+    } 
+
+    document.getElementById("bienEtreLight").onmouseout = function (){
+        document.getElementById("bienEtre").style.display = "none";
+        document.getElementById("explication-bienEtre").style.opacity = "0";
+    }
+
+    document.getElementById("bienMangerLight").onmouseover = function (){
+        document.getElementById("bienManger").style.display = "flex"; 
+        document.getElementById("explication-bienManger").style.opacity = "1";
+    }
+
+
+}
