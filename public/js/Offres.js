662fe9e805a9b520050c1023c54fb4f621b8b680
Morgane Broutet <morgane.broutet@gmail.com>
2020-01-30 00:00:33 +0100

    add precise slide links from OnePage to offres page & correct content

--- public/js/Offres.js
+++ public/js/Offres.js
@@ -4,36 +4,53 @@ var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 var sliderImgWrapper = document.getElementById('sliderImgWrapper');
 var numberImg = document.getElementById('controlNumber');
+var screenWidth = window.innerWidth;
+var hash = window.location.hash;
+
+if (screenWidth >= 425) {
+    var anchors = document.querySelectorAll("#ancreDecouverte, #ancreEssentielle");
+    anchors.forEach((anchor) => {
+        anchor.removeAttribute("id");
+    })
+}
+
+if (hash === "#ancreDecouverte") {
+    goToSlide(1);
+} else if (hash === "#ancreEssentielle") {
+    goToSlide(2);
+}
+
+
 
 next.onclick = function() {
-nextSlide();
+    nextSlide();
 };
 
 previous.onclick = function() {
-previousSlide();
+    previousSlide();
 };
 
 function nextSlide() {
-goToSlide(currentSlide+1);
+    goToSlide(currentSlide+1);
 }
 
 function previousSlide() {
-goToSlide(currentSlide-1);
+    goToSlide(currentSlide-1);
 }
 
 function goToSlide(n) {
-slides[currentSlide].className = 'slideText offresText';
-currentSlide = (n+slides.length)%slides.length;
-slides[currentSlide].className = 'slideText showing offresText';
+    slides[currentSlide].className = 'slideText offresText';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slideText showing offresText';
 
-if (currentSlide === 1) {
-    sliderImgWrapper.style.marginLeft = '-35vw';
-    numberImg.src = "public/img/2sur3.png";
-} else if (currentSlide === 2) {
-    sliderImgWrapper.style.marginLeft = '-70vw';
-    numberImg.src = "public/img/3sur3.png";
-} else if (currentSlide === 0) {
-    sliderImgWrapper.style.marginLeft = '0';
-    numberImg.src = "public/img/1sur3.png";
-}
+    if (currentSlide === 1) {
+        sliderImgWrapper.style.marginLeft = '-35vw';
+        numberImg.src = "public/img/2sur3.png";
+    } else if (currentSlide === 2) {
+        sliderImgWrapper.style.marginLeft = '-70vw';
+        numberImg.src = "public/img/3sur3.png";
+    } else if (currentSlide === 0) {
+        sliderImgWrapper.style.marginLeft = '0';
+        numberImg.src = "public/img/1sur3.png";
+    }
 }
