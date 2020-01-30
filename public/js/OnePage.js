1d179593f6399e0deff84d633877dcecafce49c0
Julie-Brun <brun.julie91@gmail.com>
2020-01-30 16:34:10 +0100

    Final Timeline

--- public/js/OnePage.js
+++ public/js/OnePage.js
@@ -2,43 +2,70 @@ $(document).ready(function(){
 
     // Timeline
 
-    // var container = $(".timelineDot");
+    //Actions au click
+    $(".timelineDot").click(function(e){ 
+        e.preventDefault();
+        $(".timelineDot").removeClass("activeDot");
+        $(".timelineDot").siblings(".timelineContent").children(".pContent").removeClass("stayContent");
+        $(this).addClass("activeDot");
+        $(this).siblings(".timelineContent").children(".pContent").addClass("stayContent");
 
-    // // Effet de hover sur la Timeline
-    // $(container).hover(function(){
-    //     $(this).siblings(".timelineContent").children(".content").animate({
-    //         opacity: 1
-    //     }, 500);
-    // }, function(){
-    //     $(this).siblings(".timelineContent").children(".content").animate({
-    //         opacity: 0
-    //     }, 500);
-    // });
+    //Actions lorsque la souris est dessus
+    }).mouseenter(function(){ 
+        $(this).siblings(".timelineContent").children(".pContent").addClass("activeContent");
 
-    // // Effet de click sur la Timeline
-    // $(container).click(function(){
-    //     $(this).css("opacity", "1");
-    //     $(this).siblings(".timelineContent").children(".content").css("opacity", "1");
-    // });
+    //Action lorsque la souris part
+    }).mouseleave(function(){ 
+        $(this).siblings(".timelineContent").children(".pContent").removeClass("activeContent");
+    });
 
-
-    $(document).on({
-        "mouseenter": function(){
-            $(this).siblings(".timelineContent").children(".content").animate({
-                opacity: 1
-            }, 500);
-        },
-        "mouseleave": function(){
-            $(this).siblings(".timelineContent").children(".content").animate({
-                opacity: 0
-            }, 500);
-        },
-        "mousedown": function(){
-            $(".timelineDot").removeClass("activeDot");
-            $(this).addClass("activeDot");
-            $(this).siblings(".timelineContent").children(".content").addClass("activeContent");
+    // Smooth scroll et saut vers la catégorie cliquée
+    function scrollTo(target) {
+        if( target.length ) {
+            $("html, body").stop().animate({scrollTop: target.offset().top}, 1500);
         }
-    }, ".timelineDot");
+    }
+
+    $("#section1").click(function(e){
+        e.preventDefault();
+        scrollTo($("#accueil"));
+    })
+
+    $("#section2").click(function(e){
+        e.preventDefault();
+        scrollTo($("#piliersSection"));
+    })
+
+    $("#section3").click(function(e){
+        e.preventDefault();
+        scrollTo($("#potagers"));
+    })
+
+    $("#section4").click(function(e){
+        e.preventDefault();
+        scrollTo($("#formules"));
+    })
+
+    $("#section5").click(function(e){
+        e.preventDefault();
+        scrollTo($("#supplement"));
+    })
+
+    // Highlight de la section en cours de lecture
+
+    $(window).on("scroll", function() {
+        $(".target").each(function() {
+            if($(window).scrollTop() >= $(this).offset().top) {
+                var id = $(this).attr("id");
+                $("#timeline .timelineContainer a").removeClass("activeDot");
+                $("#timeline .timelineContainer a").siblings(".timelineContent").children(".pContent").removeClass("stayContent");
+                $("#timeline .timelineContainer a[href='#"+ id +"']").addClass("activeDot");
+                $("#timeline .timelineContainer a[href='#"+ id +"']").siblings(".timelineContent").children(".pContent").addClass("stayContent");
+            }
+        });
+    });
+
+    // $("#nav nav a[href=#"+ id +"]").addClass("active");
 
 
 })
