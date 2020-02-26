79591ad52663eea8e2f0bd89a477a416ebf0f2ee
Morgane Broutet <morgane.broutet@gmail.com>
2020-02-26 18:23:04 +0100

    add menu burger correction

--- public/js/Offres.js
+++ public/js/Offres.js
@@ -1,3 +1,16 @@
+const button = document.getElementById("burgerMenu"); 
+const menu = document.getElementById("menuSideBar");
+const buttonSideB = document.getElementById("burgerSideBar"); 
+
+button.addEventListener("click",function(){
+    menu.classList.toggle("open"); 
+})
+
+buttonSideB.addEventListener("click", function(){
+    menu.classList.toggle("open");
+})
+
+
 var slides = document.querySelectorAll('#slidesText .slideText');
 var currentSlide = 0;
 var next = document.getElementById('next');
